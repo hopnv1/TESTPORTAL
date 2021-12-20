a4c3e794d06dba6b87c5b705fe26e92e14e6af7e
HOP NGUYEN <nguyenvanhop.nbi@gmail.com>
2021-12-20 23:49:59 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,6 +37,7 @@
             @endif
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                    <h1>Test Portal</h1>
                 </div>
